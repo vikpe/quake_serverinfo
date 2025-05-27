2e4db7398f6743009f451130cf0fa7cbeb0ab01b
Viktor Persson <viktor.persson@arcsin.se>
2025-05-27 12:10:18 +0200

    release: 0.10.0
    - Enhanced Settings parsing
    - Added code coverage
    - Updated dependencies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# quake_serverinfo [![Test](https://github.com/vikpe/quake_serverinfo/actions/workflows/test.yml/badge.svg?branch=main)](https://github.com/vikpe/quake_serverinfo/actions/workflows/test.yml) [![crates](https://img.shields.io/crates/v/quake_serverinfo)](https://crates.io/crates/quake_serverinfo) [![docs.rs](https://img.shields.io/docsrs/quake_serverinfo)](https://docs.rs/quake_serverinfo/)
+# quake_serverinfo [![Test](https://github.com/vikpe/quake_serverinfo/actions/workflows/test.yml/badge.svg?branch=main)](https://github.com/vikpe/quake_serverinfo/actions/workflows/test.yml) [![codecov](https://codecov.io/gh/vikpe/quake_serverinfo/graph/badge.svg?token=KwNnQ0ICcS)](https://codecov.io/gh/vikpe/quake_serverinfo) [![crates.io](https://img.shields.io/crates/v/quake_serverinfo)](https://crates.io/crates/quake_serverinfo) [![docs.rs](https://img.shields.io/docsrs/quake_serverinfo)](https://docs.rs/quake_serverinfo/)
 
-> Parse QuakeWorld serverinfo strings
+> A Rust crate for parsing QuakeWorld serverinfo strings
 
 ## Usage
 
@@ -17,12 +17,17 @@ assert_eq!(settings.matchtag, Some("kombat".to_string()));
 ```rust
 pub struct Settings {
     pub admin: Option<String>,
+    pub broadcast: Option<i32>,
+    pub city: Option<String>,
+    pub coords: Option<String>,
+    pub countrycode: Option<String>,
     pub deathmatch: Option<i32>,
     pub epoch: Option<i32>,
     pub fpd: Option<i32>,
     pub fraglimit: Option<i32>,
     pub gamedir: Option<String>,
     pub hostname: Option<String>,
+    pub hostport: Option<String>,
     pub ktxmode: Option<String>,
     pub ktxver: Option<String>,
     pub map: Option<String>,
@@ -35,8 +40,8 @@ pub struct Settings {
     pub pm_ktjump: Option<i32>,
     pub progs: Option<String>,
     pub qvm: Option<String>,
-    pub status: Option<String>,
     pub serverdemo: Option<String>,
+    pub status: Option<String>,
     pub sv_antilag: Option<i32>,
     pub teamplay: Option<i32>,
     pub timelimit: Option<i32>,
@@ -47,4 +52,4 @@ pub struct Settings {
 
 ## See also
 
-* [quake_infostring](https://github.com/vikpe/quake_infostring) - Parse QuakeWorld info strings
+- [quake_infostring](https://github.com/vikpe/quake_infostring) - A Rust crate for parsing generic QuakeWorld info strings
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
 //! # quake_serverinfo
-//! Parse QuakeWorld serverinfo strings
+//!
+//! A parser for QuakeWorld server info strings, converting into a typed [`Settings`] struct.
+//!
+//! This crate can optionally derive serialization and deserialization using `serde`.
+//!
+//! # Examples
+//! ```
+//! use quake_serverinfo::Settings;
+//!
+//! let settings = Settings::from(r#"\maxfps\77\matchtag\kombat"#);
+//! assert_eq!(settings.maxfps, Some(77));
+//! assert_eq!(settings.matchtag, Some("kombat".to_string()));
+//! ```
 
 use std::collections::HashMap;
 use std::fmt::Display;
@@ -7,6 +19,19 @@ use std::fmt::Display;
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
+/// Represents parsed server info.
+///
+/// Each field is an optional setting parsed from a raw info string or map,
+/// matching known keys used in server metadata.
+///
+/// # Examples
+/// ```
+/// use quake_serverinfo::Settings;
+///
+/// let settings = Settings::from(r#"\maxfps\77\matchtag\kombat"#);
+/// assert_eq!(settings.maxfps, Some(77));
+/// assert_eq!(settings.matchtag, Some("kombat".to_string()));
+/// ```
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Settings {
@@ -49,26 +74,18 @@ impl Display for Settings {
     }
 }
 
-/// # Examples
-/// ```
-/// use quake_serverinfo::Settings;
-///
-/// let settings = Settings::from(r#"\maxfps\77\matchtag\kombat"#);
-/// assert_eq!(settings.maxfps, Some(77));
-/// assert_eq!(settings.matchtag, Some("kombat".to_string()));
-/// ```
 impl From<&HashMap<String, String>> for Settings {
     fn from(value: &HashMap<String, String>) -> Self {
         Self {
             admin: map_get_string(value, "*admin"),
-            broadcast: map_get_int(value, "broadcast"),
+            broadcast: map_get_i32(value, "broadcast"),
             city: map_get_string(value, "city"),
             coords: map_get_string(value, "coords"),
             countrycode: map_get_string(value, "countrycode"),
-            deathmatch: map_get_int(value, "deathmatch"),
-            epoch: map_get_int(value, "epoch"),
-            fpd: map_get_int(value, "fpd"),
-            fraglimit: map_get_int(value, "fraglimit"),
+            deathmatch: map_get_i32(value, "deathmatch"),
+            epoch: map_get_i32(value, "epoch"),
+            fpd: map_get_i32(value, "fpd"),
+            fraglimit: map_get_i32(value, "fraglimit"),
             gamedir: map_get_string(value, "*gamedir"),
             hostname: map_get_string(value, "hostname"),
             hostport: map_get_string(value, "hostport"),
@@ -76,28 +93,40 @@ impl From<&HashMap<String, String>> for Settings {
             ktxver: map_get_string(value, "ktxver"),
             map: map_get_string(value, "map"),
             matchtag: map_get_string(value, "matchtag"),
-            maxclients: map_get_int(value, "maxclients"),
-            maxfps: map_get_int(value, "maxfps"),
-            maxspectators: map_get_int(value, "maxspectators"),
+            maxclients: map_get_i32(value, "maxclients"),
+            maxfps: map_get_i32(value, "maxfps"),
+            maxspectators: map_get_i32(value, "maxspectators"),
             mode: map_get_string(value, "mode"),
-            needpass: map_get_int(value, "needpass"),
-            pm_ktjump: map_get_int(value, "pm_ktjump"),
+            needpass: map_get_i32(value, "needpass"),
+            pm_ktjump: map_get_i32(value, "pm_ktjump"),
             progs: map_get_string(value, "*progs"),
             qvm: map_get_string(value, "*qvm"),
             serverdemo: map_get_string(value, "serverdemo"),
             status: map_get_string(value, "status"),
-            sv_antilag: map_get_int(value, "sv_antilag"),
-            teamplay: map_get_int(value, "teamplay"),
-            timelimit: map_get_int(value, "timelimit"),
+            sv_antilag: map_get_i32(value, "sv_antilag"),
+            teamplay: map_get_i32(value, "teamplay"),
+            timelimit: map_get_i32(value, "timelimit"),
             version: map_get_string(value, "*version"),
-            z_ext: map_get_int(value, "*z_ext"),
+            z_ext: map_get_i32(value, "*z_ext"),
         }
     }
 }
 
 impl From<&str> for Settings {
     fn from(value: &str) -> Self {
-        Self::from(&quake_infostring::to_hashmap(value))
+        Self::from(&quake_infostring::parse_key_values(value))
+    }
+}
+
+impl From<&String> for Settings {
+    fn from(value: &String) -> Self {
+        Settings::from(value.as_str())
+    }
+}
+
+impl From<String> for Settings {
+    fn from(value: String) -> Self {
+        Settings::from(value.as_str())
     }
 }
 
@@ -108,10 +137,10 @@ impl From<&[u8]> for Settings {
 }
 
 fn map_get_string(map: &HashMap<String, String>, key: &str) -> Option<String> {
-    map.get(key).map(|v| v.to_string())
+    map.get(key).cloned()
 }
 
-fn map_get_int(map: &HashMap<String, String>, key: &str) -> Option<i32> {
+fn map_get_i32(map: &HashMap<String, String>, key: &str) -> Option<i32> {
     map.get(key)?.parse().ok()
 }
 
@@ -121,14 +150,14 @@ mod tests {
 
     use super::*;
 
-    const INFO_STR: &str = r#"\maxfps\77\pm_ktjump\1\*version\MVDSV 0.36\*z_ext\511\*admin\suom1 <suom1@irc.ax>\ktxver\1.42\sv_antilag\2\maxspectators\12\hostname\QUAKE.SE KTX:28501\status\Standby\teamplay\2\*gamedir\qw\fpd\206\mode\2on2\*qvm\so\*progs\so\maxclients\4\timelimit\10\deathmatch\3\map\maphub_v1"#;
+    const INFO_STR: &str = r#"\maxfps\77\pm_ktjump\1\*version\MVDSV 0.36\*z_ext\511\*admin\suom1 <suom1@irc.ax>\ktxver\1.42\sv_antilag\2\maxspectators\12\hostname\QUAKE.SE KTX:28501\status\Standby\teamplay\2\*gamedir\qw\fpd\206\mode\2on2\*qvm\so\*progs\so\maxclients\4\timelimit\10\deathmatch\3\map\maphub_v1\broadcast\1"#;
 
     #[test]
     fn test_display() {
         let settings = Settings::from(INFO_STR);
         assert_eq!(
             settings.to_string(),
-            r#"Settings { admin: Some("suom1 <suom1@irc.ax>"), broadcast: None, city: None, coords: None, countrycode: None, deathmatch: Some(3), epoch: None, fpd: Some(206), fraglimit: None, gamedir: Some("qw"), hostname: Some("QUAKE.SE KTX:28501"), hostport: None, ktxmode: None, ktxver: Some("1.42"), map: Some("maphub_v1"), matchtag: None, maxclients: Some(4), maxfps: Some(77), maxspectators: Some(12), mode: Some("2on2"), needpass: None, pm_ktjump: Some(1), progs: Some("so"), qvm: Some("so"), serverdemo: None, status: Some("Standby"), sv_antilag: Some(2), teamplay: Some(2), timelimit: Some(10), version: Some("MVDSV 0.36"), z_ext: Some(511) }"#
+            r#"Settings { admin: Some("suom1 <suom1@irc.ax>"), broadcast: Some(1), city: None, coords: None, countrycode: None, deathmatch: Some(3), epoch: None, fpd: Some(206), fraglimit: None, gamedir: Some("qw"), hostname: Some("QUAKE.SE KTX:28501"), hostport: None, ktxmode: None, ktxver: Some("1.42"), map: Some("maphub_v1"), matchtag: None, maxclients: Some(4), maxfps: Some(77), maxspectators: Some(12), mode: Some("2on2"), needpass: None, pm_ktjump: Some(1), progs: Some("so"), qvm: Some("so"), serverdemo: None, status: Some("Standby"), sv_antilag: Some(2), teamplay: Some(2), timelimit: Some(10), version: Some("MVDSV 0.36"), z_ext: Some(511) }"#
         );
     }
 
@@ -139,43 +168,61 @@ mod tests {
             ("map".to_string(), "dm2".to_string()),
         ]);
 
-        let expected = Settings {
-            maxfps: Some(77),
-            map: Some("dm2".to_string()),
-            ..Default::default()
-        };
-
-        assert_eq!(Settings::from(&map), expected);
+        assert_eq!(
+            Settings::from(&map),
+            Settings {
+                maxfps: Some(77),
+                map: Some("dm2".to_string()),
+                ..Default::default()
+            }
+        );
     }
 
     #[test]
     fn test_from_str() {
-        let settings = Settings::from(INFO_STR);
-        assert_eq!(settings.admin, Some("suom1 <suom1@irc.ax>".to_string()));
-        assert_eq!(settings.broadcast, None);
-        assert_eq!(settings.city, None);
-        assert_eq!(settings.coords, None);
-        assert_eq!(settings.countrycode, None);
-        assert_eq!(settings.deathmatch, Some(3));
-        assert_eq!(settings.fpd, Some(206));
-        assert_eq!(settings.gamedir, Some("qw".to_string()));
-        assert_eq!(settings.hostname, Some("QUAKE.SE KTX:28501".to_string()));
-        assert_eq!(settings.hostport, None);
-        assert_eq!(settings.ktxver, Some("1.42".to_string()));
-        assert_eq!(settings.map, Some("maphub_v1".to_string()));
-        assert_eq!(settings.maxclients, Some(4));
-        assert_eq!(settings.maxfps, Some(77));
-        assert_eq!(settings.maxspectators, Some(12));
-        assert_eq!(settings.mode, Some("2on2".to_string()));
-        assert_eq!(settings.pm_ktjump, Some(1));
-        assert_eq!(settings.progs, Some("so".to_string()));
-        assert_eq!(settings.qvm, Some("so".to_string()));
-        assert_eq!(settings.status, Some("Standby".to_string()));
-        assert_eq!(settings.sv_antilag, Some(2));
-        assert_eq!(settings.teamplay, Some(2));
-        assert_eq!(settings.timelimit, Some(10));
-        assert_eq!(settings.version, Some("MVDSV 0.36".to_string()));
-        assert_eq!(settings.z_ext, Some(511));
+        assert_eq!(
+            Settings::from(INFO_STR),
+            Settings {
+                admin: Some("suom1 <suom1@irc.ax>".to_string()),
+                broadcast: Some(1),
+                deathmatch: Some(3),
+                fpd: Some(206),
+                gamedir: Some("qw".to_string()),
+                hostname: Some("QUAKE.SE KTX:28501".to_string()),
+                ktxver: Some("1.42".to_string()),
+                map: Some("maphub_v1".to_string()),
+                maxclients: Some(4),
+                maxfps: Some(77),
+                maxspectators: Some(12),
+                mode: Some("2on2".to_string()),
+                pm_ktjump: Some(1),
+                progs: Some("so".to_string()),
+                qvm: Some("so".to_string()),
+                status: Some("Standby".to_string()),
+                sv_antilag: Some(2),
+                teamplay: Some(2),
+                timelimit: Some(10),
+                version: Some("MVDSV 0.36".to_string()),
+                z_ext: Some(511),
+                ..Default::default()
+            }
+        );
+    }
+
+    #[test]
+    fn test_from_string_ref() {
+        assert_eq!(
+            Settings::from(&INFO_STR.to_string()),
+            Settings::from(INFO_STR)
+        )
+    }
+
+    #[test]
+    fn test_from_string() {
+        assert_eq!(
+            Settings::from(INFO_STR.to_string()),
+            Settings::from(INFO_STR)
+        )
     }
 
     #[test]
